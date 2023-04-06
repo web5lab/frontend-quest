f9d9a35f7da41d12c65f9089c3b6d19cb9c27c82
shiva kumar <shivakumar@shivas-MacBook-Air.local>
2023-04-06 21:18:31 +0530

    up

--- src/componet/common/nav/Nav.js
+++ src/componet/common/nav/Nav.js
@@ -1,52 +1,93 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./nav.css";
-// import { BsDiscord, BsTwitter } from "react-icons/bs";
-import { DynamicContextProvider, DynamicWidget } from "@dynamic-labs/sdk-react";
+import { Button } from "@chakra-ui/react";
+import Web3 from 'web3';
 
-class Nav extends Component {
-  state = { clicked: false };
-  handleclick = () => {
-    this.setState({
-      clicked: !this.state.clicked,
-    });
+function Nav() {
+  const [clicked, setClicked] = useState(false);
+  const [web3, setWeb3] = useState(null);
+  const [address, setAddress] = useState('');
+  const [message, setMessage] = useState('test message');
+  const [signature, setSignature] = useState('');
+
+  const handleClick = () => {
+    setClicked(!clicked);
   };
-  render() {
-    return (
-      <div className="startdivNav">
-        <ul id="list" className={this.state.clicked ? "#list active" : "#list"}>
-          <li>
-            <NavLink to="/login" className="navbar">
-              Set Up Quest
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/MyQuest" className="navbar">
-              Explore Quest
-            </NavLink>
-          </li>
 
-          <li>
-            <NavLink className="navbarBtn">
-              <DynamicContextProvider
-                settings={{
-                  environmentId: "cf2ed575-790e-4904-a493-47e429ffe4e8",
-                }}
-              >
-                <DynamicWidget />
-              </DynamicContextProvider>
-            </NavLink>
-          </li>
-        </ul>
-        <div className="icon" onClick={this.handleclick}>
-          <i
-            id="bar"
-            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
-          ></i>
-        </div>
+  const handleWalletConnect = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.enable();
+        const web3 = new Web3(window.ethereum);
+        const accounts = await web3.eth.getAccounts();
+        setWeb3(web3);
+        setAddress(accounts[0]);
+        await handleSignMessage();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const handleSignMessage = async () => {
+    const messageToSign = message.trim();
+    if (web3 && address && messageToSign) {
+      try {
+        const signature = await web3.eth.personal.sign(messageToSign, address, '');
+        setSignature(signature);
+        sendSignedMessage(signature);
+        console.log(signature);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const sendSignedMessage = async (signature) => {
+    const apiUrl = 'http://localhost:3009/user/metamaskAuth';
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message, signature ,address}),
+      });
+      const responseData = await response.json();
+      console.log(responseData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="startdivNav">
+      <ul id="list" className={clicked ? "#list active" : "#list"}>
+        <li>
+          <NavLink to="/login" className="navbar">
+            Set Up Quest
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/MyQuest" className="navbar">
+            Explore Quest
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink className="navbarBtn">
+            {/* wallet connect used here */}
+            <Button onClick={handleWalletConnect}>Connect Wallet</Button>
+            {/* wallet connect function from here */}
+          </NavLink>
+        </li>
+      </ul>
+      <div className="icon" onClick={handleClick}>
+        <i id="bar" className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Nav;
